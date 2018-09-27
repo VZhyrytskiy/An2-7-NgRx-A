012fa2fff2c6234225026ab8a6e597e31a847deb
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2018-09-27 18:23:13 +0300

    ngrx entity

--- src/app/core/+store/tasks/tasks.reducer.ts
+++ src/app/core/+store/tasks/tasks.reducer.ts
@@ -1,5 +1,5 @@
 import { TasksActionTypes, TasksActions } from './tasks.actions';
-import { TasksState, initialTasksState } from './tasks.state';
+import { taskAdapter, TasksState, initialTasksState } from './tasks.state';
 
 import { TaskModel } from './../../../tasks/models/task.model';
 
@@ -11,7 +11,6 @@ export function tasksReducer(
 
   switch (action.type) {
     case TasksActionTypes.GET_TASKS: {
-      console.log('GET_TASKS action being handled!');
       return {
         ...state,
         loading: true
@@ -19,18 +18,16 @@ export function tasksReducer(
     }
 
     case TasksActionTypes.GET_TASKS_SUCCESS: {
-      console.log('GET_TASKS_SUCCESS action being handled!');
-      const data = [...(<Array<TaskModel>>action.payload)];
-      return {
+      const tasks = [...(<Array<TaskModel>>action.payload)];
+
+      return taskAdapter.addAll(tasks, {
         ...state,
-        data,
         loading: false,
         loaded: true
-      };
+      });
     }
 
     case TasksActionTypes.GET_TASKS_ERROR: {
-      console.log('GET_TASKS_ERROR action being handled!');
       const error = action.payload;
       return {
         ...state,
@@ -40,79 +37,34 @@ export function tasksReducer(
       };
     }
 
-    case TasksActionTypes.CREATE_TASK: {
-      console.log('CREATE_TASK action being handled!');
-      return { ...state };
-    }
-
     case TasksActionTypes.CREATE_TASK_SUCCESS: {
-      console.log('CREATE_TASK_SUCCESS action being handled!');
       const task = { ...(<TaskModel>action.payload) };
-      const data = [...state.data, task];
 
-      return {
-        ...state,
-        data
-      };
-    }
-
-    case TasksActionTypes.CREATE_TASK_ERROR: {
-      console.log('CREATE_TASK_ERROR action being handled!');
-      const error = action.payload;
-      return {
-        ...state,
-        error
-      };
-    }
-
-    case TasksActionTypes.UPDATE_TASK: {
-      console.log('UPDATE_TASK action being handled!');
-      return { ...state };
+      return taskAdapter.addOne(task, state);
     }
 
     case TasksActionTypes.UPDATE_TASK_SUCCESS: {
-      console.log('UPDATE_TASK_SUCCESS action being handled!');
       const task = { ...(<TaskModel>action.payload) };
-      const data = [...state.data];
-      const index = data.findIndex(t => t.id === task.id);
 
-      data[index] = task;
-
-      return {
-        ...state,
-        data
-      };
-    }
-
-    case TasksActionTypes.UPDATE_TASK_ERROR: {
-      console.log('UPDATE_TASK_ERROR action being handled!');
-      const error = action.payload;
-      return {
-        ...state,
-        error
-      };
-    }
-
-    case TasksActionTypes.DELETE_TASK: {
-      console.log('DELETE_TASK action being handled!');
-      return { ...state };
+      return taskAdapter.updateOne(
+        {
+          id: task.id,
+          changes: task
+        },
+        state
+      );
     }
 
     case TasksActionTypes.DELETE_TASK_SUCCESS: {
-      console.log('DELETE_TASK_SUCCESS action being handled!');
       const task = { ...(<TaskModel>action.payload) };
-      const data = state.data.filter(t => t.id !== task.id);
 
-      return {
-        ...state,
-        data
-      };
+      return taskAdapter.removeOne(task.id, state);
     }
 
+    case TasksActionTypes.CREATE_TASK_ERROR:
+    case TasksActionTypes.UPDATE_TASK_ERROR:
     case TasksActionTypes.DELETE_TASK_ERROR: {
-      console.log('DELETE_TASK_ERROR action being handled!');
       const error = action.payload;
-
       return {
         ...state,
         error
@@ -120,7 +72,6 @@ export function tasksReducer(
     }
 
     default: {
-      console.log('UNKNOWN_TASK action being handled!');
       return state;
     }
   }
