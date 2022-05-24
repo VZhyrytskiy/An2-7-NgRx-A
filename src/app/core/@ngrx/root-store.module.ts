bb0a279e5cf4471e65db05687b3bf5949912dfb8
Vitaliy Zhyrytskyy <vitaliy_zhyrytskyy@epam.com>
2022-05-24 11:27:12 +0300

    fix

--- src/app/core/@ngrx/root-store.module.ts
+++ src/app/core/@ngrx/root-store.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-// @Ngrx
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
 import { TasksStoreModule } from './tasks/tasks-store.module';
 import { metaReducers } from './meta-reducers';
 import { environment } from './../../../environments/environment';
